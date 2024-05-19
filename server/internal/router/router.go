ab581f8b08e8e27ce4428b34d767633f19f17b79
Alan <alanpramil7@gmail.com>
2024-05-19 11:07:45 +0530

    frontend

--- server/internal/router/router.go
+++ server/internal/router/router.go
@@ -2,19 +2,33 @@ package router
 
 import (
 	"github.com/alanpramil7/go-chat/internal/user"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"time"
 )
 
 var r *gin.Engine
 
-func InitRouter(userHandler *user.Handler){
-  r = gin.Default()
+func InitRouter(userHandler *user.Handler) {
+	r = gin.Default()
 
-  r.POST("/signup", userHandler.CreateUser)
-  r.POST("/login", userHandler.Login)
-  r.GET("/logout", userHandler.Logout)
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST"},
+		AllowHeaders:     []string{"Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "http://localhost:3000"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
+
+	r.POST("/signup", userHandler.CreateUser)
+	r.POST("/login", userHandler.Login)
+	r.GET("/logout", userHandler.Logout)
 }
 
-func Start(addr string) error{
-  return r.Run(addr)
+func Start(addr string) error {
+	return r.Run(addr)
 }
